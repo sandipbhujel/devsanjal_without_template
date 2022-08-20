a7ea710eab6e202d13cbcf49a29ee60d25fb81ae
sandipbhujel <bhujelsandip34@gmail.com>
2022-08-20 14:16:52 +0545

    updating url

--- devsanjal/settings.py
+++ devsanjal/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-qx5hu6c-@rh(c^in5_6iqjly_(^r=w&)+hy!xxq1(@v$24yd!r'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
